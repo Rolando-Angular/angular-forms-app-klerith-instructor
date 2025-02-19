bc1cdeffd4d44bc6843a3742772a75a45df294bc
rolandoandre123 <rbardalespriale@gmail.com>
2025-02-18 21:06:37 -0500

    add : creat structure's project

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
+
+const routes: Route[] = [];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes),
+  ],
+  exports: [
+    RouterModule,
+  ],
+})
+export class AuthRoutingModule {
+
+}
---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reactive-basic-page',
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent {
+
+}
